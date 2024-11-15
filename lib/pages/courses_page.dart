dd54ee7f29aaed0d15d3071e4582dd48383a4ffa
DenzelHuang <denzel.huang@student.president.ac.id>
2024-11-15 15:56:23 +0700

    Added base use case pages
    Includes one singular button in the homepage that redirects the practice page. Themes are not working for some reason.

---
+++ lib/pages/courses_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CoursesPage extends StatelessWidget {
+  const CoursesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Courses Page")),
+    );
+  }
+}
---
+++ lib/pages/dictionary_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DictionaryPage extends StatelessWidget {
+  const DictionaryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dictionary Page")),
+    );
+  }
+}
